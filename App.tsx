30f1ebf7bba448a95192ffaf1c6796956b42d3c0
Dragon2000v <shmatko064@gmail.com>
2024-08-19 01:34:47 +0300

    add temp auth

--- App.tsx
+++ App.tsx
@@ -3,18 +3,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import List from './app/screens/List';
 import Details from './app/screens/Details';
 import Login from './app/screens/Login';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect, useState } from 'react';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  // onAuthStateChanged(FIREBASE_AUTH, user => {
-  //   if (user) {
-  //   } else {
-  //   }
-  // });
+  // const [authenticated, setAuthenticated] = useState(false);
+  // useEffect(() => {
+  //   onAuthStateChanged;
+  // }, []);
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="My Todos">
         <Stack.Screen name="My Todos" component={List} />
         <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -1,12 +1,23 @@
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 import React, { useState } from 'react';
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const signUp = async () => {};
-  const signIn = async () => {};
+  const auth = getAuth();
+  const signUp = async () => {
+    const after = await createUserWithEmailAndPassword(auth, email, password);
+    alert('Check your email!');
+  };
+  const signIn = async () => {
+    const user = await signInWithEmailAndPassword(auth, email, password);
+  };
   return (
     <View style={styles.container}>
       <TextInput
@@ -33,9 +44,11 @@ export default Login;
 const styles = StyleSheet.create({
   container: {
     marginHorizontal: 20,
+    flexDirection: 'column',
+    paddingVertical: 20,
   },
   input: {
-    flex: 1,
+    marginVertical: 4,
     height: 50,
     borderWidth: 1,
     borderRadius: 4,
